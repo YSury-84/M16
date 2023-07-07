5eeb44c01f33d133e614ea77ca03896182f18709
YSury-84 <Yury.Kolchurin@evraz.com>
2023-07-07 12:07:05 +0700

    Зафиксировано задание модуля 16

---
+++ Calc.Test/Class1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void CalculatorAdditionalReturnValue()
+        {
+            var calc = new Calculator();
+            int a = calc.Additional(2, 3);
+            Assert.True(a == 5);
+        }
+
+        [Test]
+        public void CalculatorSubtractionReturnValue()
+        {
+            var calc = new Calculator();
+            int s = calc.Subtraction(2, 3);
+            Assert.True(s == -1);
+        }
+
+        [Test]
+        public void CalculatorMiltiplicationReturnValue()
+        {
+            var calc = new Calculator();
+            int m = calc.Miltiplication(2, 3);
+            Assert.True(m == 6);
+        }
+
+        [Test]
+        public void CalculatorDivisionReturnValue()
+        {
+            var calc = new Calculator();
+            int d = calc.Division(2, 3);
+            Assert.True(d == 0);
+        }
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calculator!");
+            Console.WriteLine(" 2 + 2 = 5 !");
+            Console.ReadKey();
+        }
+    }
+
+    public class Calculator
+    {c
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
